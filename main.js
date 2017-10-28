2de2db413c837dcda836552a786c2601878ca62e
Jonathan Carson <carsonjd@colorado.edu>
2017-10-28 13:20:01 -0600

    saving for atom restart

--- main.js
+++ main.js
@@ -4,37 +4,50 @@ $('document').ready(function(){
   var menuOpen = false;
   $('#menu').click(function(){
     if(menuOpen = false){
-      //$('.drop-main').style.display('grid');
+      menuOpen = true;
       $('.drop-main').animate({
-        'top': '-100%'
+        'top': '-10%'
       },300);
     } else {
+      menuOpen = false;
       $('.drop-main').animate({
-        'top': '100%'
+        'top': '20%'
       },300);
     }
   })
 
-  var requestUrl = 'https://api.mapbox.com/v4/mapbox.dark/-76.9,38.9,5/400x200@2x.png?access_token=pk.eyJ1IjoiY2Fyc29uamQiLCJhIjoiY2o3Z2lkeHhqMTkzZzMybzIydXN0OXVyMSJ9.Fb8VNtwKGcp3cV9Xev1vfQ'
+  // var requestUrl = ''
+  //
+  // var xhr = $.getJSON(requestUrl);
+  // xhr.done(function(data){
+  //   console.log(data)
+  // })
 
-  var xhr = $.getJSON(requestUrl);
-  xhr.done(function(data){
-    console.log(data)
-  })
-
-  mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fyc29uamQiLCJhIjoiY2o3Z2lkeHhqMTkzZzMybzIydXN0OXVyMSJ9.Fb8VNtwKGcp3cV9Xev1vfQ';
-
-  $('body').append(new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-streets-v10',
-    center: [-77.034084, 38.909671],
-    zoom: 5
-  }));
-
-
-
-
-//mapbox.mapbox-terrain-v2
+  // $.get('https://www.reddit.com/r/aww.json')
+  // .done(function (response) {
+  //   //What to do if HTTP was successful
+  //   localStorage.setItem('redditData', JSON.stringify(response));
+  // })
+  // .fail(function (error) {
+  //   //What to do if HTTP failed
+  //   JSON.parse(localStorage.getItem('redditData'));
+  //   console.log('redditData');
+  // })
 
 
-})
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fyc29uamQiLCJhIjoiY2o5YmxuYXkyMWVjMTMzbzdsajJnc3kycyJ9.xoTkOio_DVGEpXsE97I3Zg';
+
+  var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/carsonjd/cj9bo5zhx4i4p2rmk9c5xrsax',
+  center: [-77.034084, 38.909671],
+  zoom: 5
+  });
+  console.log(map);
+
+
+
+
+
+});
